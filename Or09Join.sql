ff452bece315af490af72cc4b13d07eda63861fc
GSunwoo <ace9sun@naver.com>
2025-04-08 11:06:33 +0900

    4/8

--- Or09Join.sql
+++ Or09Join.sql
@@ -442,5 +442,133 @@ where
     city = 'South San Francisco'
 order by employee_id;
 
+/****************************************
+과제
+****************************************/
+
+/*
+1. inner join 방식중 오라클방식을 사용하여 first_name 이 Janette 인 사원의 부서ID와 부서명을 출력하시오.
+출력목록] 부서ID, 부서명
+*/
+
+select
+    d.department_id, department_name
+from employees e, departments d
+where e.department_id = d.department_id
+    and first_name='Janette';
+/*
+    오라클 방식은 표준방식에서 inner join 대신 콤마를 이용해서 테이블을 조인하고 on절 대신 where절에 조인될 컬럼을 명시한다.
+*/
+
+/*
+2. inner join 방식중 SQL표준 방식을 사용하여 사원이름과 함께 그 사원이 소속된 부서명과 도시명을 출력하시오
+출력목록] 사원이름, 부서명, 도시명
+*/
+
+select
+    first_name, department_name, city
+from employees e 
+    inner join departments d 
+        on e.department_id=d.department_id
+    inner join locations l
+        on d.location_id = l.location_id;
+
+/*
+3. 사원의 이름(FIRST_NAME)에 'A'가 포함된 모든사원의 이름과 부서명을 출력하시오.
+출력목록] 사원이름, 부서명
+*/
+
+
+/*
+4. “city : Toronto / state_province : Ontario” 에서 근무하는 모든 사원의 이름, 업무명, 부서번호 및 부서명을 출력하시오.
+출력목록] 사원이름, 업무명, 부서ID, 부서명
+*/
+
+select
+    first_name, job_title, department_id, department_name
+from locations 
+    inner join departments using(location_id)
+    inner join employees using(department_id)
+    inner join jobs using(job_id)
+where city='Toronto' and state_province='Ontario';
+
+/*
+5. Equi Join(등가조인)을 사용하여 커미션(COMMISSION_PCT)을 받는 모든 사원의 이름, 부서명, 도시명을 출력하시오. 
+출력목록] 사원이름, 부서ID, 부서명, 도시명
+*/
+select
+    first_name, d.department_id, department_name, city
+from employees e, departments d, locations l
+where e.department_id=d.department_id
+    and d.location_id=l.location_id
+    and commission_pct is not null;
+    
+/*
+6. inner join과 using 연산자를 사용하여 50번 부서(DEPARTMENT_ID)에 속하는 모든 담당업무(JOB_ID)의 
+고유목록(distinct)을 부서의 도시명(CITY)을 포함하여 출력하시오.
+출력목록] 담당업무ID, 부서ID, 부서명, 도시명
+*/
+select
+    distinct job_id, department_id, department_name, city
+from departments
+    inner join employees using(department_id)
+    inner join jobs using(job_id)
+    inner join locations using(location_id)
+where department_id = 50;
+
+/*
+7. 담당업무ID가 FI_ACCOUNT인 사원들의 매니져는 누구인지 출력하시오. 단, 레코드가 중복된다면 중복을 제거하시오. 
+출력목록] 이름, 성, 담당업무ID, 급여
+*/
+select
+    distinct mgr.first_name, mgr.last_name,
+    mgr.job_id, mgr.salary
+from employees clk, employees mgr
+where clk.manager_id = mgr.employee_id
+    and clk.job_id = 'FI_ACCOUNT';
+    /*
+    사원의 매니저ID와 매니저의 사원ID를 셀프조인의 조건으로 사용
+    사원의 담당업무를 조건으로 추가
+    */
+/*
+8. 각 부서의 메니져가 누구인지 출력하시오. 출력결과는 부서번호를 오름차순 정렬하시오.
+출력목록] 부서번호, 부서명, 이름, 성, 급여, 담당업무ID
+※ departments 테이블에 각 부서의 메니져가 있습니다.
+*/
+select
+    d.department_id, department_name, first_name, last_name,
+    salary, job_id
+from departments d
+    inner join employees e
+        on d.manager_id=e.employee_id
+order by d.department_id;
+
+/*
+9. 담당업무명이 Sales Manager인 사원들의 입사년도와 입사년도(hire_date)별 평균 급여를 출력하시오. 
+출력시 년도를 기준으로 오름차순 정렬하시오. 
+출력항목 : 입사년도, 평균급여
+*/
+select
+    to_char(hire_date,'yyyy') hire_year, avg(salary)
+from employees inner join jobs using(job_id)
+where job_title = 'Sales Manager'
+group by to_char(hire_date,'yyyy') /*연도별로 그룹을 묶어준다.*/
+order by hire_year desc; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
